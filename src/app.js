e9d70ea2a7ab4d2601eace60c5d9c9e18e31b86f
Alex Martin <alextravc.342@gmail.com>
2023-01-03 19:12:57 -0500

    [ADD]: docker-compose-dev.yml and docker-compose.yml added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 'use strict';
 
 import express from 'express';
+import { usersRoutes } from './routes/index.js';
 
 const app = express();
+
+// Routes
+app.use('/users', usersRoutes);
 
 export default app;
---
+++ src/routes/users.routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.json({ status: true, msg: 'GET' });
+});
+
+export default router;
