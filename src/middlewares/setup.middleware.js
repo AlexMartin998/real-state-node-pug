73d7cf3780775f7c79aa9d298c0da2a1e1e33a11
Alex Martin <alextravc.342@gmail.com>
2023-01-04 20:07:48 -0500

    [ADD]: CSRF protection added

--- src/middlewares/setup.middleware.js
+++ src/middlewares/setup.middleware.js
@@ -1,15 +1,21 @@
 'use strict';
 
 import express from 'express';
+import csrf from 'csurf';
+import cookieParser from 'cookie-parser';
 
 export const setupMiddlewares = app => {
     app.use(express.static('./src/public'));
     app.set('view engine', 'pug');
     app.set('views', './src/views');
-    app.use(express.urlencoded({ extended: false })); // leer datos del front embebido
+
+    // habilitar la lectura de datos del formulario del front embebido
+    app.use(express.urlencoded({ extended: false }));
 
     // app.use(compression()).use(helmet());
     // app.use(morgan('dev'));
 
     // Other middlewares
+    app.use(cookieParser());
+    app.use(csrf({ cookie: true }));
 };
