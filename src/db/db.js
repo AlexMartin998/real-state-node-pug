b22bf21ec0a976612a3818936103fd9bed4f74cf
Alex Martin <alextravc.342@gmail.com>
2023-01-18 09:07:19 -0500

    [ADD]: provision MySQL added - Railway

--- src/db/db.js
+++ src/db/db.js
@@ -6,12 +6,14 @@ import {
     DB_HOST,
     DB_NAME,
     DB_PASSWORD,
+    DB_PORT,
     DB_USER,
 } from '../config/index.js';
 
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST, // docker-compose-dev.yml
     dialect: DB_DIALECT,
+    port: DB_PORT,
     // dialectOptions: { ssl: {} }, //scalePlanet - online - serveles, no relaciones x fk
     define: { timestamps: true },
 
